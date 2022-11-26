d8169de37f0bddc128cb84f84e5e353d63341db7
Ruoqi Huang <huangruoqi374@gmail.com>
2022-11-26 13:08:00 -0800

    light fade out

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -11,6 +11,17 @@ public class PlayerManager : MonoBehaviour
     Animator anim;
     PlayerMovement pm;
 
+    float deathTime = 1f;
+    float deathTimer = 1f;
+    float intensity;
+    
+    public Renderer point;
+    public Renderer leg1;
+    public Renderer leg2;
+    public Renderer leg3;
+    public Renderer leg4;
+    public Light halo;
+
     void Awake() {
         if (instance == null) {
             instance = this;
@@ -27,12 +38,23 @@ public class PlayerManager : MonoBehaviour
     {
         anim = player.GetComponent<Animator>();
         pm = GetComponent<PlayerMovement>();
+        intensity = halo.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (point != null && deathTimer < deathTime) {  
+            float r, b, g, a = r = b = g = 1f - deathTimer / deathTime;
+            Color customColor = new Color(r, b, g, a);
+            point.material.SetColor("_EmissionColor", customColor);
+            leg1.material.SetColor("_EmissionColor", customColor);
+            leg2.material.SetColor("_EmissionColor", customColor);
+            leg3.material.SetColor("_EmissionColor", customColor);
+            leg4.material.SetColor("_EmissionColor", customColor);
+            halo.intensity = intensity - deathTimer / deathTime * intensity / 2;
+        }
+        deathTimer += Time.deltaTime;
     }
 
     public void SetPosition(Vector3 p) {
@@ -65,6 +87,7 @@ public class PlayerManager : MonoBehaviour
             // UI: choose levels
             // GameManager.instance.LoadLevel();
         anim.SetBool("isDead", true);
+        deathTimer = 0f;
         pm.alive = false;
         pm.StopMoving();
     }
